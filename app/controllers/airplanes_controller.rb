fee8f7011327081b8c9b83708d45769cf4df0352
Jennifer Humphrey <jen.e.humphrey@gmail.com>
2015-02-27 21:50:06 -0700

    set up airplanes and reg controllers

---
+++ app/controllers/airplanes_controller.rb
@@ -0,0 +1,45 @@
+class AirplanesController < ApplicationController
+
+  def index
+    @airplanes = Airplane.all
+  end
+
+  def new
+    @airplane = Airplane.new
+  end
+
+  def show
+    @airplane = Airplane.find(params[:id])
+  end
+
+  def edit
+    @airplane= Airplane.find(params[:id])
+  end
+
+  def create
+    airplane_params = params.require(:airplane).permit(
+      :name,
+      :pilot,
+      )
+    @airplane = Airplane.new(airplane_params)
+    @airplane.save
+    redirect_to airplanes_path
+  end
+
+  def update
+    @airplane = Airplane.find(params[:id])
+    airplane_params = params.require(:airplane).permit(
+      :name,
+      :pilot,
+    )
+    @airplane.update(airplane_params)
+    redirect_to airplanes_path
+  end
+
+  def destroy
+    @airplane = Airplane.find(params[:iid])
+    @airplane.destroy(airplane_params)
+    redirect_to airplanes_path
+  end
+
+end
